c75c14d2211d01b1df9be27a8092230321b7e34c
CrumrineCoder <crumrinecoding@gmail.com>
2018-01-20 15:09:48 -0500

    Formatting inline done, classes added, img error removed

--- js/index.js
+++ js/index.js
@@ -1,78 +1,71 @@
 // Add each stream to an anchor point with the classes online/offline, mature/nonmature/ and its language
 // Add them with Angular. stream in Streams, have a new array
 // Refresh the streams every 5 minutes?
-
 var streamNames = ["Starladder5", "Resonance22", "FreeCodeCamp"];
+var app = angular.module('stream', []);
+app.controller('streamController', function($scope) {
+    $scope.Streams = [];
 
-        var app = angular.module('stream', []);
-        app.controller('streamController', function($scope) {
-                $scope.Streams = [];
+    function displayStreams(name, data) {
+        if (data.stream) {
+            var obj = {
+                game: data.stream.game,
+                viewers: data.stream.viewers,
+                language: data.stream.channel.broadcaster_language,
+                name: data.stream.channel.display_name,
+                followers: data.stream.channel.followers,
+                logo: data.stream.channel.logo,
+                mature: data.stream.channel.mature,
+                online: true,
+                url: data.stream.channel.url,
+                preview: data.stream.preview.large,
+                title: data.stream.channel.status
+            };
+            $scope.$apply(function() {
+                $scope.Streams.push(obj);
+            });
+        } else {
+            getOfflineStreamData(name);
+        }
+    }
+    getStreamData();
 
-                function displayStreams(name, data) {
-                    if (data.stream) {
-						var obj = {
-							game: data.stream.game,
-							viewers: data.stream.viewers,
-							language: data.stream.channel.broadcaster_language,
-							name: data.stream.channel.display_name,
-							followers: data.stream.channel.followers,
-							logo: data.stream.channel.logo,
-							mature: data.stream.channel.mature,
-							online: true,
-							url: data.stream.channel.url,
-							preview: data.stream.preview.large,
-							title: data.stream.channel.status
-						};
-						$scope.$apply(function() {
-							$scope.Streams.push(obj);
-						});
-                        console.log(name + " is online");
-                        console.log(data)
-                    } else {
-                        console.log(name + " is offline");
-                        getOfflineStreamData(name);
-                    }
+    function getOfflineStreamData(name) {
+        $.ajax({
+            type: "GET",
+            url: "https://wind-bow.glitch.me/twitch-api/channels/" + name,
+            success: function(data) {
+                var obj = {
+                    game: data.game,
+                    language: data.broadcaster_language,
+                    name: data.display_name,
+                    followers: data.followers,
+                    logo: data.logo,
+                    mature: data.mature,
+                    online: false,
+                    url: data.url,
+                    preview: data.video_banner,
+                    title: data.status
                 }
-                getStreamData();
+                $scope.$apply(function() {
+                    $scope.Streams.push(obj);
+                });
+            }
+        });
+    }
 
-                function getOfflineStreamData(name) {
-                    $.ajax({
-                        type: "GET",
-                        url: "https://wind-bow.glitch.me/twitch-api/channels/" + name,
-                        success: function(data) {
-							var obj = {
-								game: data.game,
-								language: data.broadcaster_language, 
-								name: data.display_name,
-								followers: data.followers,
-								logo: data.logo,
-								mature: data.mature,
-								online: false,
-								url: data.url,
-								preview: data.video_banner,
-								title: data.status
-							}
-							$scope.$apply(function() {
-								$scope.Streams.push(obj);
-							});
-                            console.log(data);
-                        }
-                    });
-                }
-
-                function getStreamData() {
-                    streamNames.forEach(function(data, i) {
-                        $.ajax({
-                            type: "GET",
-                            url: 'https://api.twitch.tv/kraken/streams/' + streamNames[i],
-                            headers: {
-                                "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
-                            },
-                            success: function(data) {
-                                displayStreams(streamNames[i], data);
-                            }
-                        });
-                    });
+    function getStreamData() {
+        streamNames.forEach(function(data, i) {
+            $.ajax({
+                type: "GET",
+                url: 'https://api.twitch.tv/kraken/streams/' + streamNames[i],
+                headers: {
+                    "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
+                },
+                success: function(data) {
+                    displayStreams(streamNames[i], data);
                 }
             });
-   
+        });
+    }
+});
