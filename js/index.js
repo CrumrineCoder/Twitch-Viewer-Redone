9e5b499264fa02d3abde12fcbfa44525aba40d0e
CrumrineCoder <crumrinecoding@gmail.com>
2018-01-21 00:09:13 -0500

    Update

--- js/index.js
+++ js/index.js
@@ -1,25 +1,23 @@
 // Allow users to see a condensed view with ng-repeat that will not show the preview or the small details
 // Search bar
+// Get the value from the user. Get the data. Push the obj into the array and sort it. 
 // All, Online, Offline
 // Mature Only
 // Language select
 // Refresh the streams every 5 minutes? Reload button as well.
-var streamNames = ["Jerma985", "ASKLJDFALJKSDF","BabaYetu_","Starladder5", "Resonance22", "TPangolin", "FreeCodeCamp", "VooblyOfficial"];
+var streamNames = ["Jerma985", "BabaYetu_", "Starladder5", "Resonance22", "TPangolin", "FreeCodeCamp", "VooblyOfficial"];
 var app = angular.module('stream', []);
-function compare(a,b) {
-  if (a.followers < b.followers)
-    return 1;
-  if (a.followers > b.followers)
-    return -1;
-  return 0;
-}
 
+function compare(a, b) {
+    if (a.followers < b.followers) return 1;
+    if (a.followers > b.followers) return -1;
+    return 0;
+}
 app.controller('streamController', function($scope) {
     $scope.Streams = [];
 
     function displayStreams(name, data) {
         if (data.stream) {
-			console.log("displayStreams: " + name);  
             var obj = {
                 game: data.stream.game,
                 viewers: data.stream.viewers,
@@ -33,13 +31,11 @@ app.controller('streamController', function($scope) {
                 preview: data.stream.preview.large,
                 title: data.stream.channel.status
             };
-			console.log(obj);
             $scope.$apply(function() {
                 $scope.Streams.push(obj);
-				$scope.Streams.sort(compare);
+                $scope.Streams.sort(compare);
             });
         } else {
-			console.log("displayStreams: " + name); 
             getOfflineStreamData(name);
         }
     }
@@ -50,26 +46,26 @@ app.controller('streamController', function($scope) {
             type: "GET",
             url: "https://wind-bow.glitch.me/twitch-api/channels/" + name,
             success: function(data) {
-				if(data.status !="404"){
-					console.log("getOfflineStreamData: " + name); 
-                var obj = {
-                    game: data.game,
-                    language: data.broadcaster_language,
-                    name: data.display_name,
-                    followers: data.followers,
-                    logo: data.logo,
-                    mature: data.mature,
-                    online: false,
-                    url: data.url,
-                    preview: data.video_banner,
-                    title: data.status
+                if (data.status != "404") {
+                    var obj = {
+                        game: data.game,
+                        language: data.broadcaster_language,
+                        name: data.display_name,
+                        followers: data.followers,
+                        logo: data.logo,
+                        mature: data.mature,
+                        online: false,
+                        url: data.url,
+                        preview: data.video_banner,
+                        title: data.status
+                    }
+                    $scope.$apply(function() {
+                        $scope.Streams.push(obj);
+                        $scope.Streams.sort(compare);
+                    });
+                } else {
+                    alert("Streamer not found. Search again.");
                 }
-					console.log(obj);
-                $scope.$apply(function() {
-                    $scope.Streams.push(obj);
-					$scope.Streams.sort(compare);
-                });
-				}
             }
         });
     }
@@ -83,7 +79,6 @@ app.controller('streamController', function($scope) {
                     "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
                 },
                 success: function(data) {
-					console.log("getStreamData: " + streamNames[i]); 
                     displayStreams(streamNames[i], data);
                 }
             });
