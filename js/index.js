0becbb08ce7a94cd2681a383ec05a05fd3ea89cc
CrumrineCoder <crumrinecoding@gmail.com>
2018-01-21 15:10:59 -0500

    Backup

--- js/index.js
+++ js/index.js
@@ -17,12 +17,11 @@ function compare(a, b) {
 app.controller('streamController', function($scope) {
     $scope.Streams = [];
 	$scope.Filter = new Object();
-	$scope.Filter.online = {
-		true: true, 
-		false: false
-    };
-	$scope.OrderFilter = 'online';
-	console.log($scope.Filter);
+  $scope.Filter.online = {
+    true: true, 
+    false: false
+  };
+
 	$('#twitchSearch').submit(function(e) {
 		e.preventDefault();
 		var searchValue = $("#searchBar").val();
@@ -107,7 +106,58 @@ app.controller('streamController', function($scope) {
     }
 });
 
+/*
+app.filter('searchFilter', function($filter) {
+  return function(items, searchfilter) {
+    console.log("item");
+console.log(items); 	
+    var isSearchFilterEmpty = true;
+	// Figure out if there's a filter
+    angular.forEach(searchfilter, function(searchstring) {
+      if (searchstring != null && searchstring != "") {
+        isSearchFilterEmpty = false;
+      }
+    });
+	// If there's a filter
+    if (!isSearchFilterEmpty) {
+	// The filtered streams
+      var result = [];
+	  // For each stream. items = all streams
+      angular.forEach(items, function(item) {
+        var isFound = false;
+		// Go through each value in the Angular, eg. the logo, true, and the title of the streamer
+		// item = current stream
+        angular.forEach(item, function(term, key) {
+          if (term != null && !isFound) {
+            term = term.toString();
+            term = term.toLowerCase();
+			// Add the stream to the list of streams if it matches the filter
+			// Term = current item, like twitch.tv/freecodecamp, freecodecamp, en. It's the value
+			// Key = current item, like url, preview, title. It's the key, duh. 
+            angular.forEach(searchfilter, function(searchstring) {
+              if (searchstring != "" && term.indexOf(searchstring) != -1 && !isFound) {
+                result.push(item);
+                isFound = true;
+				
+              }
+            });
+          }
+        });
+      });
+	 // If things are filtered
+	  console.log(result); 
+      return result;
+	  // If things aren't filtered
+    } else {
+	  console.log("items");
+	  console.log(items);
+      return items;
+    }
+  }
+}); */
 
+
+// Global search filter
 app.filter('searchFilter', function($filter) {
   return function(items, searchfilter) {
     var isSearchFilterEmpty = true;
@@ -125,8 +175,8 @@ app.filter('searchFilter', function($filter) {
             term = term.toString();
             term = term.toLowerCase();
             angular.forEach(searchfilter, function(searchstring) {
-            console.log(searchstring);
-   
+          
+              searchstring = searchstring.toLowerCase();
               if (searchstring != "" && term.indexOf(searchstring) != -1 && !isFound) {
                 result.push(item);
                 isFound = true;
@@ -135,6 +185,7 @@ app.filter('searchFilter', function($filter) {
           }
         });
       });
+
       return result;
     } else {
       return items;
