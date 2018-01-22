fb3bf3eb4ec5e629a9d4e8de29628ca6c0f9050d
CrumrineCoder <crumrinecoding@gmail.com>
2018-01-22 13:37:24 -0500

    Bug fixes
    Purple background now extends all the way down and was/is is fixed.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,36 @@
 // Allow users to see a condensed view with ng-repeat that will not show the preview or the small details
-// Remove button
+// Search bar
+// Get the data. Push the obj into the array and sort it. 
 // All, Online, Offline
 // Mature Only
 // Language select
 // Refresh the streams every 5 minutes? Reload button as well.
-var streamNames = ["imaqtpie","FreeCodeCamp", "Jerma985", "DDRJake", "handmade_hero"];
-var app = angular.module('stream', ['ngMaterial']);
+var streamNames = ["Jerma985", "BabaYetu_", "Starladder5", "Resonance22", "TPangolin", "FreeCodeCamp", "VooblyOfficial"];
+var app = angular.module('stream', []);
 
 function compare(a, b) {
     if (a.followers < b.followers) return 1;
     if (a.followers > b.followers) return -1;
     return 0;
 }
-function onlineOnly(){
-	
-}
+
+
 app.controller('streamController', function($scope) {
     $scope.Streams = [];
-    
-	
-    $('#twitchSearch').submit(function(e) {
-        e.preventDefault();
-        var searchValue = $("#searchBar").val();
-        $.ajax({
-            type: "GET",
-            url: 'https://api.twitch.tv/kraken/streams/' + searchValue,
-            headers: {
-                "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
-            },
-            success: function(data) {
-                displayStreams(searchValue, data);
-            }
-        });
-    });
-
+	$('#twitchSearch').submit(function(e) {
+		e.preventDefault();
+		var searchValue = $("#searchBar").val();
+		 $.ajax({
+                type: "GET",
+                url: 'https://api.twitch.tv/kraken/streams/' + searchValue,
+                headers: {
+                    "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
+                },
+                success: function(data) {
+                    displayStreams(searchValue, data);
+                }
+         });
+	});
     function displayStreams(name, data) {
         if (data.stream) {
             var obj = {
@@ -76,7 +73,7 @@ app.controller('streamController', function($scope) {
                         url: data.url,
                         preview: data.video_banner,
                         title: data.status
-                    };
+                    }
                     $scope.$apply(function() {
                         $scope.Streams.push(obj);
                         $scope.Streams.sort(compare);
