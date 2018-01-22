d39bc927bf140b36a4e571e2fb9d6f370d6765e1
CrumrineCoder <crumrinecoding@gmail.com>
2018-01-22 13:26:39 -0500

    Swap from online to status
    Errors: was/is, background is only purple until you scroll down

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 // Mature Only
 // Language select
 // Refresh the streams every 5 minutes? Reload button as well.
-var streamNames = ["FreeCodeCamp", "Jerma985", "DDRJake", "handmade_hero"];
+var streamNames = ["imaqtpie","FreeCodeCamp", "Jerma985", "DDRJake", "handmade_hero"];
 var app = angular.module('stream', ['ngMaterial']);
 
 function compare(a, b) {
@@ -12,82 +12,13 @@ function compare(a, b) {
     if (a.followers > b.followers) return -1;
     return 0;
 }
+function onlineOnly(){
+	
+}
 app.controller('streamController', function($scope) {
     $scope.Streams = [];
-    $scope.filter = {};
-    $scope.categories = ['status'];
-
-        $scope.addProps = function(obj, array) {
-            console.log('addProps');
-            console.log(obj);
-            console.log(array);
-            if (typeof array === 'undefined') {
-                return false;
-            }
-            return array.reduce(function(prev, item) {
-                if (typeof item[obj] === 'undefined') {
-                    return prev;
-                }
-                return prev + parseFloat(item[obj]);
-            }, 0);
-        }
-        $scope.getItems = function(obj, array) {
-            console.log("getItems");
-            console.log(obj);
-            console.log(array);
-            return (array || []).map(function(w) {
-                return w[obj];
-            }).filter(function(w, idx, arr) {
-                if (typeof w === 'undefined') {
-                    return false;
-                }
-                return arr.indexOf(w) === idx;
-            });
-        };
-        // matching with AND operator
-        $scope.filterByPropertiesMatchingAND = function(data) {
-            console.log("filterByPropertiesAnd");
-            console.log(data);
-            var matchesAND = true;
-            for (var obj in $scope.filter) {
-                if ($scope.filter.hasOwnProperty(obj)) {
-                    if (noSubFilter($scope.filter[obj])) continue;
-                    if (!$scope.filter[obj][data[obj]]) {
-                        matchesAND = false;
-                        break;
-                    }
-                }
-            }
-            return matchesAND;
-        };
-        // matching with OR operator
-        $scope.filterByPropertiesMatchingOR = function(data) {
-            console.log("filterByPropertiesOr");
-            console.log(data);
-            var matchesOR = true;
-            for (var obj in $scope.filter) {
-                if ($scope.filter.hasOwnProperty(obj)) {
-                    if (noSubFilter($scope.filter[obj])) continue;
-                    if (!$scope.filter[obj][data[obj]]) {
-                        matchesOR = false;
-                    } else {
-                        matchesOR = true;
-                        break;
-                    }
-                }
-            }
-            return matchesOR;
-        };
-
-        function noSubFilter(obj) {
-            console.log("noSubFilter");
-            console.log(obj);
-            for (var key in obj) {
-                if (obj[key]) return false;
-            }
-            return true;
-        }
     
+	
     $('#twitchSearch').submit(function(e) {
         e.preventDefault();
         var searchValue = $("#searchBar").val();
@@ -113,7 +44,7 @@ app.controller('streamController', function($scope) {
                 followers: data.stream.channel.followers,
                 logo: data.stream.channel.logo,
                 mature: data.stream.channel.mature,
-                online: true,
+                status: "online",
                 url: data.stream.channel.url,
                 preview: data.stream.preview.large,
                 title: data.stream.channel.status
@@ -141,7 +72,7 @@ app.controller('streamController', function($scope) {
                         followers: data.followers,
                         logo: data.logo,
                         mature: data.mature,
-                        online: false,
+                        status: "offline",
                         url: data.url,
                         preview: data.video_banner,
                         title: data.status
