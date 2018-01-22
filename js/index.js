e4657d1ab5e8d940fec20e30808e96d62a32338e
CrumrineCoder <crumrinecoding@gmail.com>
2018-01-22 11:19:22 -0500

    New take on filter

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 // Mature Only
 // Language select
 // Refresh the streams every 5 minutes? Reload button as well.
-var streamNames = [ "FreeCodeCamp", "Jerma985", "DDRJake", "handmade_hero"];
+var streamNames = ["FreeCodeCamp", "Jerma985", "DDRJake", "handmade_hero"];
 var app = angular.module('stream', []);
 
 function compare(a, b) {
@@ -12,24 +12,97 @@ function compare(a, b) {
     if (a.followers > b.followers) return -1;
     return 0;
 }
-
-
 app.controller('streamController', function($scope) {
     $scope.Streams = [];
-	$('#twitchSearch').submit(function(e) {
-		e.preventDefault();
-		var searchValue = $("#searchBar").val();
-		 $.ajax({
-                type: "GET",
-                url: 'https://api.twitch.tv/kraken/streams/' + searchValue,
-                headers: {
-                    "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
-                },
-                success: function(data) {
-                    displayStreams(searchValue, data);
+    $scope.filter = {};
+    $scope.categories = ['status'];
+
+        $scope.addProps = function(obj, array) {
+            console.log('addProps');
+            console.log(obj);
+            console.log(array);
+            if (typeof array === 'undefined') {
+                return false;
+            }
+            return array.reduce(function(prev, item) {
+                if (typeof item[obj] === 'undefined') {
+                    return prev;
                 }
-         });
-	});
+                return prev + parseFloat(item[obj]);
+            }, 0);
+        }
+        $scope.getItems = function(obj, array) {
+            console.log("getItems");
+            console.log(obj);
+            console.log(array);
+            return (array || []).map(function(w) {
+                return w[obj];
+            }).filter(function(w, idx, arr) {
+                if (typeof w === 'undefined') {
+                    return false;
+                }
+                return arr.indexOf(w) === idx;
+            });
+        };
+        // matching with AND operator
+        $scope.filterByPropertiesMatchingAND = function(data) {
+            console.log("filterByPropertiesAnd");
+            console.log(data);
+            var matchesAND = true;
+            for (var obj in $scope.filter) {
+                if ($scope.filter.hasOwnProperty(obj)) {
+                    if (noSubFilter($scope.filter[obj])) continue;
+                    if (!$scope.filter[obj][data[obj]]) {
+                        matchesAND = false;
+                        break;
+                    }
+                }
+            }
+            return matchesAND;
+        };
+        // matching with OR operator
+        $scope.filterByPropertiesMatchingOR = function(data) {
+            console.log("filterByPropertiesOr");
+            console.log(data);
+            var matchesOR = true;
+            for (var obj in $scope.filter) {
+                if ($scope.filter.hasOwnProperty(obj)) {
+                    if (noSubFilter($scope.filter[obj])) continue;
+                    if (!$scope.filter[obj][data[obj]]) {
+                        matchesOR = false;
+                    } else {
+                        matchesOR = true;
+                        break;
+                    }
+                }
+            }
+            return matchesOR;
+        };
+
+        function noSubFilter(obj) {
+            console.log("noSubFilter");
+            console.log(obj);
+            for (var key in obj) {
+                if (obj[key]) return false;
+            }
+            return true;
+        }
+    
+    $('#twitchSearch').submit(function(e) {
+        e.preventDefault();
+        var searchValue = $("#searchBar").val();
+        $.ajax({
+            type: "GET",
+            url: 'https://api.twitch.tv/kraken/streams/' + searchValue,
+            headers: {
+                "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
+            },
+            success: function(data) {
+                displayStreams(searchValue, data);
+            }
+        });
+    });
+
     function displayStreams(name, data) {
         if (data.stream) {
             var obj = {
@@ -54,7 +127,7 @@ app.controller('streamController', function($scope) {
         }
     }
     getStreamData();
-	
+
     function getOfflineStreamData(name) {
         $.ajax({
             type: "GET",
@@ -72,7 +145,7 @@ app.controller('streamController', function($scope) {
                         url: data.url,
                         preview: data.video_banner,
                         title: data.status
-                    }
+                    };
                     $scope.$apply(function() {
                         $scope.Streams.push(obj);
                         $scope.Streams.sort(compare);
