e2a8115ffc98daaa60653837fd42174ec01474a7
CrumrineCoder <crumrinecoding@gmail.com>
2019-02-05 10:23:51 -0500

    Refresh button now has cooldown, error message update, remove vestigial code from comments

--- js/index.js
+++ js/index.js
@@ -49,6 +49,10 @@ function showAll() {
     $('#allButton').addClass('active');
     display = 'all';
 }
+
+window.isButtonXAllowed = true;
+
+
 // Our angular controller. 
 app.controller('streamController', function ($scope) {
     // The streams to be shown tot he user
@@ -57,117 +61,72 @@ app.controller('streamController', function ($scope) {
     $scope.Condensed = false;
     // Change streanNames to those of the current Streamers and run through the entire process again to renew the data. 
     $scope.refresh = function () {
-        streamNames = [];
-        //   console.log($scope.Streams);
-        for (var i = 0; i < $scope.Streams.length; i++) {
-            streamNames.push({ user: $scope.Streams[i].user, status: $scope.Streams[i].status });
-        }
-        console.log($scope.Streams);
-        console.log(streamNames);
-        //  $scope.Streams = [];
-        //  getStreamData();
-        function update(obj) {
-            console.log(obj);
-            var result = $scope.Streams.filter(object => {
-                return object.user_id === obj.user_id
-            });
-            var index = $scope.Streams.indexOf(result);
-            result[0].game = obj.game;
-            result[0].viewers = obj.viewers;
-            result[0].preview = obj.preview;
-            result[0].title = obj.title;
-            console.log(result);
-            if (index !== -1) {
-                $scope.$apply(function () {
-                    $scope.Streams[index] = result[0];
-                });
+        if (window.isButtonXAllowed) {
+            window.isButtonXAllowed = false;
+            streamNames = [];
+            //   console.log($scope.Streams);
+            for (var i = 0; i < $scope.Streams.length; i++) {
+                streamNames.push({ user: $scope.Streams[i].user, status: $scope.Streams[i].status });
             }
-        }
 
-        for (var i = 0; i < streamNames.length; i++) {
-            if (streamNames[i].status == "online") {
-                $.ajax({
-                    type: "GET",
-                    url: 'https://api.twitch.tv/helix/streams?user_login=' + streamNames[i].user,
-                    headers: {
-                        "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
-                    },
-                    success: function (data) {
-                        console.log(data);
-                        if (data.data.length > 0) {
-                            var obj = {
-                                game: data.data[0].game_id,
-                                viewers: data.data[0].viewer_count,
-                                user_id: data.data[0].user_id,
-                                preview: data.data[0].thumbnail_url,
-                                title: data.data[0].title
-                            }
-                            update(obj);
-                        }
-                    }/* function (data) {
-                        console.log($scope.Streams);
-                        console.log(i);
-                        var temp = $scope.Streams[i];
-                        console.log(temp); */
-                    /*
-                         
-                                    };
-                    
-                                    var temp = obj.preview.replace("{width}x{height}", "1920x1080")
-                                    obj.preview = temp;
-                                            if (data.data.length < 1) {
-                                                if (containsObject(storage.display_name, $scope.Streams)) {
-                                                    alert("Stream is already shown.")
-                                                } else {
-                                                    //   alert("Stream is offline");
-                                                    var obj = {
-                                                        desc: storage.description,
-                                                        user: storage.id,
-                                                        status: "offline",
-                                                        profile: storage.profile_image_url,
-                                                        offline: storage.offline_image_url,
-                                                        views: storage.view_count
-                                                    }
-                            
-                                                    var temp = obj.profile.replace("{width}x{height}", "1920x1080")
-                                                    obj.profile = temp;
-                            
-                                                    getFollowers(obj);
-                                                }
-                                            } else {
-                                                if (containsObject(data.data[0].user_name, $scope.Streams)) {
-                                                    alert("Stream is already shown.")
-                                                } else {
-                                                    //    alert("Stream is online");
-                                                    displayInitialStreams(data.data);
-                                                }
-                                            } */
-                    //    displayStreams(searchValue, data);
-                    //   },
-                    ,
-                    error: function (data) {
-                        alert("Stream doesn't exist, sorry!")
-                    }
+            function update(obj) {
+                var result = $scope.Streams.filter(object => {
+                    return object.user_id === obj.user_id
                 });
+                var index = $scope.Streams.indexOf(result);
+                result[0].game = obj.game;
+                result[0].viewers = obj.viewers;
+                result[0].preview = obj.preview;
+                result[0].title = obj.title;
+                if (index !== -1) {
+                    $scope.$apply(function () {
+                        $scope.Streams[index] = result[0];
+                    });
+                }
             }
-            /*    $.ajax({
-                    type: "GET",
-                    url: 'https://api.twitch.tv/helix/users?login=' + streamNames[i].user,
-                    headers: {
-                        "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
-                    },
-                    success: function (data) {
-                        console.log(data);
-                        if (data.data.length < 1) {
-                            alert("Streamer doesn't exist, sorry!")
-                        } else {
-                            displayStreams(streamNames[i], data);
+
+            for (var i = 0; i < streamNames.length; i++) {
+                if (streamNames[i].status == "online") {
+                    $.ajax({
+                        type: "GET",
+                        url: 'https://api.twitch.tv/helix/streams?user_login=' + streamNames[i].user,
+                        headers: {
+                            "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
+                        },
+                        success: function (data) {
+                            if (data.data.length > 0) {
+                                var obj = {
+                                    game: data.data[0].game_id,
+                                    viewers: data.data[0].viewer_count,
+                                    user_id: data.data[0].user_id,
+                                    preview: data.data[0].thumbnail_url,
+                                    title: data.data[0].title
+                                }
+                                var temp = obj.preview.replace("{width}x{height}", "1920x1080")
+                                obj.preview = temp;
+                                $.ajax({
+                                    type: "GET",
+                                    url: 'https://api.twitch.tv/helix/games?id=' + obj.game,
+                                    headers: {
+                                        "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
+                                    },
+                                    success: function (data) {
+                                        obj.game = data.data[0].name;
+                                        update(obj);
+                                    }
+                                });
+                            }
+                        },
+                        error: function (data) {
+                            alert("Either that stream doesn't exist, or the Twitch API has been called too much recently (sorry, not my choice!)")
                         }
-                    },
-                    error: function (data) {
-                        alert("Stream doesn't exist, sorry!")
-                    }
-                }); */
+                    });
+                }
+            }
+            alert("Streams refreshed!");
+            setTimeout(function () { window.isButtonXAllowed = true }, 60000);
+        } else{
+            alert("Please wait 1 minute before refreshing again (Twitch API won't allow for rapid refreshes, sorry!");
         }
     }
     // On submission of the search form, add the streamer if found. 
@@ -188,7 +147,7 @@ app.controller('streamController', function ($scope) {
                 }
             },
             error: function (data) {
-                alert("Stream doesn't exist, sorry!")
+                alert("Either that stream doesn't exist, or the Twitch API has been called too much recently (sorry, not my choice!)")
             }
         });
     });
@@ -199,19 +158,6 @@ app.controller('streamController', function ($scope) {
     }
     // We get the initial data from the official Twitch API
     function getStreamData() {
-        /*     streamNames.forEach(function(data, i) {
-                 $.ajax({
-                     type: "GET",
-                     url: 'https://api.twitch.tv/kraken/streams/' + streamNames[i],
-                     headers: {
-                         "Client-ID": "qq6g00bkkiultjwkvpkewm5mkr44ock"
-                     },
-                     success: function(data) {
-                         displayStreams(streamNames[i], data);
-                     }
-                 });
-             }); */
-        //url: 'https://api.twitch.tv/helix/streams?first=20&game_id=509660',
         $.ajax({
             type: "GET",
             url: 'https://api.twitch.tv/helix/streams?first=3&game_id=509660',
@@ -220,6 +166,9 @@ app.controller('streamController', function ($scope) {
             },
             success: function (data) {
                 displayInitialStreams(data.data);
+            },
+            error: function (data) {
+                alert("Either that stream doesn't exist, or the Twitch API has been called too much recently (sorry, not my choice!)")
             }
         });
 
@@ -235,11 +184,15 @@ app.controller('streamController', function ($scope) {
             success: function (data) {
                 obj.game = data.data[0].name;
                 getFollowers(obj);
+            },
+            error: function (data) {
+                alert("Either that stream doesn't exist, or the Twitch API has been called too much recently (sorry, not my choice!)")
             }
         });
     }
 
     function getFollowers(obj) {
+        console.log(obj);
         $.ajax({
             type: "GET",
             url: 'https://api.twitch.tv/helix/users/follows?to_id=' + obj.user_id,
@@ -249,6 +202,9 @@ app.controller('streamController', function ($scope) {
             success: function (data) {
                 obj.followers = data.total;
                 getUserData(obj);
+            },
+            error: function (data) {
+                alert("Either that stream doesn't exist, or the Twitch API has been called too much recently (sorry, not my choice!)")
             }
         });
     }
@@ -270,6 +226,9 @@ app.controller('streamController', function ($scope) {
                 obj.profile = temp;
 
                 apply(obj);
+            },
+            error: function (data) {
+                alert("Either that stream doesn't exist, or the Twitch API has been called too much recently (sorry, not my choice!)")
             }
         });
     }
@@ -308,7 +267,6 @@ app.controller('streamController', function ($scope) {
     function displayStreams(name, data) {
         var storage = data.data[0];
         // If they are currently streaming
-
         $.ajax({
             type: "GET",
             url: 'https://api.twitch.tv/helix/streams?user_id=' + storage.id,
@@ -320,10 +278,10 @@ app.controller('streamController', function ($scope) {
                     if (containsObject(storage.display_name, $scope.Streams)) {
                         alert("Stream is already shown.")
                     } else {
-                        //   alert("Stream is offline");
+                         alert("Stream is offline");
                         var obj = {
                             desc: storage.description,
-                            user: storage.id,
+                            user_id: storage.id,
                             status: "offline",
                             profile: storage.profile_image_url,
                             offline: storage.offline_image_url,
@@ -339,96 +297,14 @@ app.controller('streamController', function ($scope) {
                     if (containsObject(data.data[0].user_name, $scope.Streams)) {
                         alert("Stream is already shown.")
                     } else {
-                        //    alert("Stream is online");
+                        alert("Stream is online");
                         displayInitialStreams(data.data);
                     }
                 }
-                //    displayStreams(searchValue, data);
             },
             error: function (data) {
-                alert("Stream doesn't exist, sorry!")
-            }
-        });
-        // Create an object of the data.
-        /*      var obj = {
-                  game: data.stream.game,
-                  viewers: data.stream.viewers,
-                  language: data.stream.channel.broadcaster_language,
-                  user: data.stream.channel.display_name,
-                  followers: data.stream.channel.followers,
-                  profile: data.stream.channel.logo,
-                  mature: data.stream.channel.mature,
-                  status: "online",
-                  url: data.stream.channel.url,
-                  preview: data.stream.preview.large,
-                  title: data.stream.channel.status
-              }; 
-              // If the stream hasn't already been put into the Streams array
-              if (!containsObject(obj, $scope.Streams)) {
-                  // Add the obj to the streams array and resort it
-                  $scope.$apply(function() {
-                      $scope.Streams.push(obj);
-                      $scope.Streams.sort(compare);
-                  });
-                  // If the current display is set to offline, then we tell the user why the stream isn't going to show up
-                  if (display == 'offline') {
-                      alert("Stream added, but only are showing Offline streams so it is not currently visible");
-                      offlineOnly();
-                  }
-              // Tell the user the stream is already added
-              } else {
-                  console.log(obj);
-                  console.log( $scope.Streams);
-                  console.log(containsObject(obj, $scope.Streams));
-                  alert("Stream is already added.");
-              }*/
-        // If the streamer isn't streaming. We do this because the Official Twitch API doesn't give that much data on offline streams, but the other API doesn't give as much on online streams so I have to use both. 
-    }
-
-    // Get the data for offline streams. 
-    function getOfflineStreamData(name) {
-        $.ajax({
-            type: "GET",
-            url: "https://wind-bow.glitch.me/twitch-api/channels/" + name,
-            success: function (data) {
-                // If the streamer is found
-                if (data.status != "404") {
-                    // Organize an object to be put into the Streams array
-                    var obj = {
-                        game: data.game,
-                        language: data.broadcaster_language,
-                        name: data.display_name,
-                        followers: data.followers,
-                        logo: data.logo,
-                        mature: data.mature,
-                        status: "offline",
-                        url: data.url,
-                        preview: data.video_banner,
-                        title: data.status
-                    }
-                    // If the object isn't already in the Streams array.
-                    if (!containsObject(obj, $scope.Streams)) {
-                        // Add the object to the Streams array and resort it
-                        $scope.$apply(function () {
-                            $scope.Streams.push(obj);
-                            $scope.Streams.sort(compare);
-                        });
-                        // If the display is set to only show online streams, then tell the user why their stream isn't showing up
-                        if (display == 'online') {
-                            alert("Stream added, but only are showing Online streams so it is not currently visible");
-                            onlineOnly();
-                        }
-                        // If the object IS already in the Streams Array, tell the user. 
-                    } else {
-                        alert("Stream is already added.");
-                    }
-                    // Tell the user the streamer isn't found if we can't find it with either API
-                } else {
-                    alert("Streamer not found. Search again.");
-                }
+                alert("Either that stream doesn't exist, or the Twitch API has been called too much recently (sorry, not my choice!)")
             }
         });
     }
-
-
 });
